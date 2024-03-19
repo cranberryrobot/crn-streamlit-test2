e2a9f9a36dffb7d4e6afaffd16d75404a9a2bc46
cranberryrobot <cranberryrobot@users.noreply.github.com>
2024-03-19 16:38:55 +0000

    Update - test

--- pages/4_MapTest.py
+++ pages/4_MapTest.py
@@ -37,7 +37,7 @@ def mapping_demo():
             "Police Data Points": pdk.Layer(
                 "HexagonLayer",
                 data=from_data_file(),
-                get_position=['location'['lattitude'], 'location'['longitude']],
+                get_position=[55, 3],
                 radius=200,
                 elevation_scale=4,
                 elevation_range=[0, 1000],
