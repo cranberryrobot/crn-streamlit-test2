53590964a6a6b10912db6840c579d6d8d26c28b4
cranberryrobot <cranberryrobot@users.noreply.github.com>
2024-03-19 16:22:04 +0000

    Added, updated new test

---
+++ pages/4_MapTest.py
@@ -0,0 +1,84 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from urllib.error import URLError
+
+import pandas as pd
+import pydeck as pdk
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+
+
+
+def mapping_demo():
+    @st.cache_data
+    def from_data_file(filename):
+        url = (
+            "https://data.police.uk/api/crimes-at-location?date=2017-02&lat=52.629729&lng=-1.131592" % filename
+        )
+        return pd.read_json(url)
+
+    try:
+        ALL_LAYERS = {
+            "Bike Rentals": pdk.Layer(
+                "HexagonLayer",
+                data=from_data_file("bike_rental_stats.json"),
+                get_position=["lon", "lat"],
+                radius=200,
+                elevation_scale=4,
+                elevation_range=[0, 1000],
+                extruded=True,
+            )
+        }
+        st.sidebar.markdown("### Map Layers")
+        selected_layers = [
+            layer
+            for layer_name, layer in ALL_LAYERS.items()
+            if st.sidebar.checkbox(layer_name, True)
+        ]
+        if selected_layers:
+            st.pydeck_chart(
+                pdk.Deck(
+                    map_style=None,
+                    initial_view_state={
+                        "latitude": 37.76,
+                        "longitude": -122.4,
+                        "zoom": 11,
+                        "pitch": 50,
+                    },
+                    layers=selected_layers,
+                )
+            )
+        else:
+            st.error("Please choose at least one layer above.")
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+
+st.set_page_config(page_title="Test", page_icon="🌍")
+st.title("This is a title")
+st.write(
+    """This is some text"""
+)
+
+mapping_demo()
+
