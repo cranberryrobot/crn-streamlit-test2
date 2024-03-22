073888c56a87b04ecc7765264da5de6c83610e54
cranberryrobot <cranberryrobot@users.noreply.github.com>
2024-03-22 17:34:52 +0000

    PF7

--- pages/4_MapTest.py
+++ pages/4_MapTest.py
@@ -35,7 +35,7 @@ def barchart():
         df = df.join(pd.json_normalize(df.location)).drop(columns=['location'])
 
         st.write(df.columns.tolist())
-        df['police_force.api_url'] = df.agg(lambda x: f"https://data.police.uk/api/locate-neighbourhood?q={x['longitude']},{x['latitude']}", axis=1)
+        df['police_force.api_url'] = df.agg(lambda x: f"https://data.police.uk/api/locate-neighbourhood?q={x['latitude']},{x['longitude']}", axis=1)
         df['police_force'] = df.apply(lambda x: pd.read_json(x['police_force.api_url']), axis=1)
 
         return df
