073d028cfc87509fb9d0be02561250bad6e55cfa
cranberryrobot <cranberryrobot@users.noreply.github.com>
2024-04-12 14:48:35 +0100

    Message

--- pages/4_MapTest.py
+++ pages/4_MapTest.py
@@ -43,7 +43,7 @@ def barchart(long, lat):
             df = df.join(pd.json_normalize(df.location))
             df['police_force_api_url'] = df.agg(lambda x: f"https://data.police.uk/api/locate-neighbourhood?q={x['latitude']},{x['longitude']}", axis=1)
             st.write(df.columns.tolist())
-            df['police_force'] = df.apply(lambda x: pd.read_json(x['police_force_api_url']), axis=1, index=[0])
+            df['police_force'] = df.apply(lambda x: pd.read_json(x['police_force_api_url']), axis=1)
             return df
         except URLError or AttributeError:
             st.error("The data with the longitudes and lattitudes indicated could not be found, or an error occurred.")
